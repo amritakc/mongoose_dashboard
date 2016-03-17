ab9fc36cfe7c33ef05114eccc5410fe96e397e12
amritakc <amritakc@gmail.com>
2016-03-17 12:08:28 -0700

    updated modularized version

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 var path = require("path");
-app.use(express.static(__dirname + "./static"));
-app.set('views', path.join(__dirname, './views'));
+app.use(express.static(path.join(__dirname, './client/static')));
+app.set('views', path.join(__dirname, './client/views'));
 app.set('view engine', 'ejs');
 
 require('./server/config/mongoose.js');
--- server/controllers/penguins.js
+++ server/controllers/penguins.js
@@ -8,34 +8,33 @@ module.exports = {
     })
   },
   create: function(req, res) {
-    var quote = new Quote({name: req.body.name, quo: req.body.quo});
     var penguin = new Penguin({name: req.body.name, age: req.body.age, hobby: req.body.hobby});
-  	penguin.save(function(err) {
+    penguin.save(function(err) {
     if(err) {
       console.log('something went wrong');
     } else { // else console.log that we did well and then redirect to the root route
       console.log('successfully added a penguin!');
       res.redirect('/');
      }
-  	})	
+    })  
    },
    showone: function(req, res){
-   	Penguin.findOne({_id:req.params.id},function(err,penguin){
+    Penguin.findOne({_id:req.params.id},function(err,penguin){
       res.render('show', {penguin:penguin});
     })
    },
    editone: function(req,res){
-   	Penguin.findOne({_id:req.params.id},function(err,penguin){
+    Penguin.findOne({_id:req.params.id},function(err,penguin){
       res.render('edit', {penguin:penguin});
     })
    },
    update: function(req, res){
-   	   Penguin.update({_id:req.params.id},{name:req.body.name, age:req.body.age, hobby:req.body.hobby},function(err,penguin){
+       Penguin.update({_id:req.params.id},{name:req.body.name, age:req.body.age, hobby:req.body.hobby},function(err,penguin){
           res.redirect('/');
     })
    },
    destroy: function(req, res){
-   		Penguin.remove({_id:req.params.id},function(err){
+      Penguin.remove({_id:req.params.id},function(err){
           res.redirect('/');
     })
    }
